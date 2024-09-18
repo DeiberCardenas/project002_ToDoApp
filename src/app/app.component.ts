414cb123ec234299cf197691301066e8cf9483a3
Deiber Cardenas <deibercardenas02@gmail.com>
2024-09-18 08:32:31 -0500

    To Do App Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Item } from './interfaces/item';
+import { ItemComponent } from './item/item.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ItemComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'project002_ToDoApp';
+  title = 'Tareitas!';
+
+  componentTitle = 'Mis tareitas!!';
+
+  filter: 'todas' | 'activa' | 'listo!' = 'todas';
+
+  allItems = [
+    { description: 'mamarle la cuca a mi mujer', done: true },
+    { description: 'comer un sandwich', done: false },
+    { description: 'comprar condones', done: false },
+    { description: 'ir donde las putas', done: true },
+  ];
+
+  get items() {
+    if (this.filter === 'todas') {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === 'listo!' ? item.done : !item.done
+    );
+  }
+
+  addItem(description: string) {
+    if (!description) return;
+
+    this.allItems.unshift({ description, done: false });
+  }
+
+  remove(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
 }
